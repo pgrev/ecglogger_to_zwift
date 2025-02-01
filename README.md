3f9bb20f87b1d650e7874e8ed2c2f452efe20ec3
Philipp <git@philippgreve.com>
2025-02-01 15:35:35 +0100

    Initial Upload

--- README.md
+++ README.md
@@ -1,3 +1,20 @@
 # ecglogger_to_zwift
 
 Adds heart rate data from ECGlogger records to Zwift activity (.fit)
+
+**Next Steps:**
+
+* Logging implementation
+* Error handling
+* Testing
+
+**How to use:**
+
+1. Download FitCSVTool from the Garmin Developer Website (https://developer.garmin.com/fit/fitcsvtool/)
+2. Unpack the FitCSVTool in the root directory of the application
+3. Export Zwift Activity-File (.fit)
+   * Login to your account on https://www.zwift.com/
+   * Find the activity, click the gear icon -> "Download Fit File"
+4. Place the .fit file in ./input/fit/ `<subfolder>`
+5. Place the .csv file(s) aka recording from ECGlogger in ./input/ecglogger/ `<subfolder>`
+6. run ecglogger_to_zwift.py
--- ecglogger_to_zwift.py
+++ ecglogger_to_zwift.py
@@ -1,7 +1,7 @@
 from os import listdir, system
+import csv
 from os.path import isfile, join
-from pandas import read_csv, DataFrame
-from datetime import datetime, timedelta
+from datetime import datetime
 
 # Input
 # The .fit file that needs heart rate should be located in ./input/fit/.
@@ -27,18 +27,17 @@ for f in ecg_file:
     if not f.endswith(".csv"): ecg_file.remove(f)
 print(ecg_file)
 
-# Convert .fit to .csv
-system(f"java -jar ./{fitcsv_path}/java/FitCSVTool.jar {fit_path}{fit_file[0]}")
-
-# Parse fit
-original = read_csv(f"{fit_path}{fit_file[0].replace(".fit",".csv")}", sep=",", low_memory=False)
-df_original = DataFrame(original)
-
 # Parse and merge ecglogger csv records
+# Garmin Epoch Time: 1989–12–31T00:00:00Z (https://developer.garmin.com/fit/cookbook/decoding-activity-files/#:~:text=Timestamps%20in%20FIT%20messages%20are,in%20the%20user's%20local%20timezone.)
 
-df_no = 0
-dataframes = []
-rows = []
+def convert_ecg_timestamp_to_zwift_timestamp(ecg_timestamp):
+    garmin_epoch_start = datetime(1989, 12, 31, 0, 0, 0)
+    ecg_seconds_utc = int(ecg_timestamp) // 1_000_000_000 -3600 # nanoseconds to seconds, UTC+1 to UTC (-1 hour)
+    garmin_epoch_unix_seconds_utc = int(datetime.timestamp(garmin_epoch_start)) # Garmin epoch (1989-12-31 00:00:00 UTC) in Unix time
+    zwift_timestamp = ecg_seconds_utc - garmin_epoch_unix_seconds_utc
+    return zwift_timestamp
+
+hr_rows = {}
 for record in ecg_file:
     f = open(f"{ecg_path}{record}")
     for row in f:
@@ -46,22 +45,60 @@ for record in ecg_file:
         else: 
             if row.split(",")[2] == "": continue # erase leftover rows without hr data
             else:
-                rows.append([datetime.fromtimestamp((int(row.split(",")[0]) // 1_000_000_000)), row.split(",")[2]])
+                zwift_timestamp = convert_ecg_timestamp_to_zwift_timestamp(row.split(",")[0])
+                hr_rows[zwift_timestamp] = row.split(",")[2]
 
-print(rows[:20])
+# Convert .fit to .csv
+system(f"java -jar ./{fitcsv_path}/java/FitCSVTool.jar {fit_path}{fit_file[0]}")
 
-# # Garmin Epoch Time: 1989–12–31T00:00:00Z (https://developer.garmin.com/fit/cookbook/decoding-activity-files/#:~:text=Timestamps%20in%20FIT%20messages%20are,in%20the%20user's%20local%20timezone.)
-# print("TEST TIMESTAMP")
-# print(datetime.fromtimestamp(1106836735))
-# zwift = 1107252091
-# print(f"zwift = {zwift}")
-# print(datetime.fromtimestamp(zwift))
-# print(datetime(2025, 1, 31, 11, 1, 0) - timedelta(seconds=zwift))
-# ecg = 1738317622493314701
-# s_ecg = ecg // 1_000_000_000
-# print(datetime.fromtimestamp(s_ecg))
-# print(f"ecg = {ecg}")
+# Parse .fit
+zwift_rows = []
+f = open(f"{fit_path}{fit_file[0].replace(".fit",".csv")}")
+for row in f:
+    zwift_rows.append(row.split(","))
 
+# Replace heart rate based on timestamp
+hr_values = []
+for row in zwift_rows:
+    if row[0] == "Data" and row[2] == "record":
+        try:
+            row[25] = hr_rows[int(row[4].strip().replace('"', ''))]
+            hr_values.append(int(row[25]))
+        except:
+            i = 1
+            while True: # Replaces missing value with next available value
+                try:
+                    row[25] = hr_rows[int(row[4].strip().replace('"', ''))+i]
+                    hr_values.append(int(row[25]))
+                    break
+                except:
+                    i += 1
+                    continue
 
-df = DataFrame(rows, columns=["time", "hr"])
-df.to_csv(f"./output/hr.csv")
+# Calcualte and enter average and max heart rate
+average_hr = sum(hr_values) // len(hr_values)
+max_hr = max(hr_values)
+
+for row in zwift_rows[-7:-1]:
+    if row[0] == "Data" and "avg_heart_rate" in row:  
+        index = row.index("avg_heart_rate") + 1
+        row[index] = average_hr
+
+for row in zwift_rows[-7:-1]:
+    if row[0] == "Data" and "max_heart_rate" in row:
+        index = row.index("max_heart_rate") + 1
+        row[index] = max_hr
+
+# remove "-characters from each field
+for row in zwift_rows: 
+    for i in range(len(row)):
+        row[i] = str(row[i]).strip().replace('"', '')
+
+output_path = "./output/hr.csv"
+with open(output_path, "w") as csvfile:
+    writer = csv.writer(csvfile, quoting=csv.QUOTE_MINIMAL)
+    for row in zwift_rows:
+        writer.writerow(row)
+
+# Convert .csv to .fit
+system(f"java -jar ./{fitcsv_path}/java/FitCSVTool.jar -c {output_path} {output_path.replace(".csv",".fit")}")
